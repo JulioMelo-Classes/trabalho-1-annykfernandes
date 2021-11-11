9bef6419a55f590eb0a0eb0a6acfe0481afaea84
Anny K Fernandes <78033190+annykfernandes@users.noreply.github.com>
2021-11-11 01:20:21 -0300

    update a messy main

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,227 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 using String = std::string;
-int main(){
-  
-    std::vector<String> menu = {"0 -> Ver scores anteriores", "1 -> Iniciar jogo"};
 
-    std::cout << menu[0] << std::endl;
-    std::cout << menu[1] << std::endl;
-
-    int comando;
-    std::cin >> comando;
-
-    if(comando == 0)
+bool verificaPalavra(char letra, string palavra)
+{
+  for(int i = 0; i<palavra.size(); i++)
+  {
+    if( letra == palavra[i])
     {
-      std::cout << "Nenhum score armazenado ainda...";
+      return true;
     }
-    else if(comando ==1)
+  }
+  return false;
+}
+
+
+int geraNivel()
+{
+  int nivel;
+  cout<<"SELECIONE UM NÍVEL:"<<endl;
+  cout<<"Digite [1] para -> FÁCIL"<<endl;
+  cout<<"Digite [2] para -> MÉDIO"<<endl;
+  cout<<"Digite [3] para -> DIFÍCIL"<<endl;
+//fazer virificação aqui da entrada
+  cin >> nivel;
+  return nivel;
+}
+
+string geraPalavra(int nivel)
+{
+  bool facil = nivel == 1;
+  bool medio = nivel == 2;
+  bool dificil = nivel == 3;
+  string palavra;
+  if(facil)
+  {
+    palavra = "LUFFY";
+  }
+  else if(medio)
+  {
+    palavra = "JINBE";
+  }
+  else if(dificil)
+  {
+    palavra = "SHIRAHOSHI";
+  }
+  cout<<palavra<<endl;
+  return palavra;
+}
+
+int validaChuteErrado(int t)
+{
+  if(t==5)
+  {
+     cout<<"  _______"<<endl;
+     cout<<" |/      |" <<endl;     
+     cout<<" |      (_)" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<"_|__________ \n" <<endl;  
+  }
+  if(t==4)
+  {
+     cout<<"  _______"<<endl;
+     cout<<" |/      |" <<endl;     
+     cout<<" |      (_)" <<endl;     
+     cout<<" |       |" <<endl;     
+     cout<<" |       |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<"_|__________ \n" <<endl;   
+  }
+  if(t==3)
+  {
+     cout<<"  _______"<<endl;
+     cout<<" |/      |" <<endl;     
+     cout<<" |      (_)" <<endl;     
+     cout<<" |      /|" <<endl;     
+     cout<<" |       |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<"_|__________ \n" <<endl;   
+  }
+  if(t==2)
+  {
+     cout<<"  _______"<<endl;
+     cout<<" |/      |" <<endl;     
+     cout<<" |      (_)" <<endl;     
+     cout<<" |      /|\\" <<endl;     
+     cout<<" |       |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<"_|__________ \n" <<endl;  
+  }
+  if(t==1)
+  {
+     cout<<"  _______"<<endl;
+     cout<<" |/      |" <<endl;     
+     cout<<" |      (_)" <<endl;     
+     cout<<" |      /|\\" <<endl;     
+     cout<<" |       |" <<endl;     
+     cout<<" |      / "<<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<"_|__________ \n" <<endl;  
+  }
+  if(t==0)
+  {
+     cout<<"  _______"<<endl;
+     cout<<" |/      |" <<endl;     
+     cout<<" |      (_)" <<endl;     
+     cout<<" |      /|\\" <<endl;     
+     cout<<" |       |" <<endl;     
+     cout<<" |      / \\"<<endl;     
+     cout<<" |" <<endl;     
+     cout<<" |" <<endl;     
+     cout<<"_|__________ \n" <<endl;  
+  }
+  
+  return 0;
+}
+
+int game(string palavra)
+{ 
+  
+  int tentativa = 6;
+  int score = 0;
+  bool nao_ganhou = true;
+  bool nao_enforcou = tentativa <= 6;
+
+  while(nao_ganhou && nao_enforcou)
+  {
+    char hint;
+    cin >> hint;
+    bool acerto = verificaPalavra(hint, palavra);
+    if(acerto)
     {
-      std::cout << "Vamos começar!";
+      score++;
+      validaChuteCorreto(palavra, hint);
     }
     else
     {
-      std::cout << "Comando inválido.";
+      score--;
+      tentativa--;
+      validaChuteErrado(tentativa);
     }
+    
+  }
+  return 0;
+}
+
+
+
+
+int main(){
+  
+  cout<<"------------------------------------"<<endl;
+  cout<<"---- BEM-VINDX AO JOGO DA FORCA ----"<<endl;
+  cout<<"------------------------------------"<<endl;
+  
+
+   cout<<"  _______"<<endl;
+   cout<<" |/      |" <<endl;     
+   cout<<" | " <<endl;     
+   cout<<" |" <<endl;     
+   cout<<" |" <<endl;     
+   cout<<" |" <<endl;     
+   cout<<" |" <<endl;     
+   cout<<" |" <<endl;     
+   cout<<"_|__________ \n" <<endl;     
+
+  bool loop = true;
+
+  while(loop == true)
+  {
+    cout<<"SELECIONE UMA OPÇÃO:"<<endl;
+    cout<<"Digite [1] para -> INICIAR"<<endl;
+    cout<<"Digite [2] para -> VER SCORES ANTERIORES"<<endl;
+    cout<<"Digite [3] para -> SAIR DO JOGO"<<endl;
+
+    int comando;
+    cin >> comando;
+
+    bool iniciar = comando == 1;
+    bool scores = comando == 2;
+    bool sair = comando == 3;
+
+    if(scores)
+    {
+      cout << " ..." << endl;
+      // puxa uma lista de scores e imprime
+      //mostra menu
+    }
+    else if(iniciar)
+    {
+      cout << "-------------------------------"<<endl;
+      cout << "------- Vamos começar! --------"<<endl;
+      cout << "-------------------------------"<<endl;
+      int nivel = geraNivel();
+      string palavra = geraPalavra(nivel);
+      game(palavra);
+      //mostra menu
+    }
+    else if(sair)
+    {
+      cout << "Fim de jogo."<<endl;
+      return 0;
+    }
+    else
+    {
+      cout << "Ops! Comando inválido.."<<endl;
+    }
+  }
+    
 
     return 0;
 }
