064fcda14043702f76d6709c115bce329f605a63
Anny K Fernandes <78033190+annykfernandes@users.noreply.github.com>
2021-11-15 03:42:32 -0300

    Update Forca.hpp

--- include/Forca.hpp
+++ include/Forca.hpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include<string>
 #include <fstream>
+#include <map>
+
 using namespace std;
  
 class Forca {
@@ -12,6 +14,21 @@ class Forca {
         enum Dificuldade{
             FACIL, MEDIO, DIFICIL
         };
+        
+        map<char, bool> hit;
+        
+        int index_pair;
+        
+        std::vector< std::pair<std::string, int> > game_on_words;
+        
+        bool n_ganhou = true;
+
+        bool n_enforcou = true;
+
+        bool n_desistiu = true;
+
+        char chute;
+
     private:
        
         std::vector< std::pair<std::string, int> > m_palavras; //<! palavras e sua ocorrência no Corpus
@@ -20,12 +37,18 @@ class Forca {
  
         std::string m_arquivo_palavras; //<! nome do arquivo contendo as palavras
  
-        Dificuldade d = Dificuldade::FACIL; //<! dificuldade do jogo
+        Dificuldade d; //<! dificuldade do jogo
  
         std::string m_palavra_atual; //<! palavra sendo testada atualmente
  
-        int m_tentativas_restantes = 6; //<! tentativas restantes
-   
+        int m_tentativas_restantes;//<! tentativas restantes
+
+        std::vector< std::pair<std::string, int> > pairs_easy;
+
+        std::vector< std::pair<std::string, int> > pairs_medium;
+
+        std::vector< std::pair<std::string, int> > pairs_hard;
+
     public:
         /**
          * Cria a classe Forca
@@ -37,21 +60,19 @@ class Forca {
          * @see eh_valido
          */
         // Forca( std::string palavras, std::string scores);
-        Forca();
-       
- 
-        /**
-         * Valida os arquivos de entrada de acordo com as especificações.
-         * Ao validar os arquivos, no caso de arquivos inválidos, este método deve retornar a 
-         * razão correspondente de acordo com as especificações.
-         * @return {T,""} se os arquivos estiverem válidos, {F,"razão"} caso contrário.
-         */
-        std::pair<bool, std::string> eh_valido();
- 
-        /**
-         * Carrega os arquivos de scores e palavras preenchendo **ao menos** a estrutura m_palavras
-         */
-        void carrega_arquivos();
+        Forca(std::vector< std::pair<std::string, int> > p);
+
+        bool nao_ganhou();
+
+        bool update_n_ganou();
+
+        bool nao_enforcou();
+
+        bool update_n_enforcou();
+
+        bool nao_desistiu();
+
+        bool update_n_desistiu();
  
         /**
          * Modifica a dificuldade do jogo.
@@ -69,45 +90,45 @@ class Forca {
          * letras que devem aparecer dependendo do nível de dificuldade.
          * @return a próxima palavra do conjunto de palavras disponível de acordo com a dificuldade atual.
          */
-        std::string proxima_palavra();
+        std::string proxima_palavra(int indx);
  
         /**
          * Retorna a palavra atual que está sendo jogada.
          * @return o valor do atributo m_palavra_atual.
          */
-        std::string get_palavra_atual();
- 
-        /**
-         * Testa se uma letra pertence á palavra atual, retorna T se a letra pertence.
-         * Este método testa se uma letra pertence à palavra atual e retorna True caso pertença. 
-         * Caso contrário, o método deve atualizar o atributo m_tentativas_restantes, decrementando 
-         * em 1, e retornar False.
-         * @param palpite uma letra, que deve ser testada se pertence à palavra.
-         * @return T se a palpite pertence à palavra, F caso contrário.
-         */
-        bool palpite(std::string palpite);
- 
-        /**
-         * Em caso de Game Over este método deve retornar T.
-         * @return T caso o m_tentativas_restantes do jogo esteja igual a 0, F caso contrário.
-         */
-        bool game_over();
+        std::string get_palavra_atual(int indx);
+
+        bool verifica_palavra();
+
+        void set_chute(char c);
+
+        void set_palavra_atual(std::string p_a);
+
+        void status_palavra();
  
         /**
          * Atualiza o número de tentativas restantes.
          * Este método é útil no caso do jogador escolher continuar o jogo.
          * @param tentativas atualiza o valor da variável m_tentativas_restantes.
          */
-        void set_tentativas_restantes(int tentativas)
-        {
-
-        }
- 
+        void set_tentativas_restantes(int tentativas);
         /**
          * Retorna a quantidade de tentativas restantes.
          * @return a quantidade de tentativas restantes.
          */
+        
         int get_tentativas_restantes();
+        /**
+         * Verifica o valor da valiável @param tentativas e desenha no console.
+         */
+
+        void update_tentativas();
+
+        int status_tentativas();
+
+        void reset_tentativas();
+
+        void verify_n_draw(int s_t);
  
 };
 
