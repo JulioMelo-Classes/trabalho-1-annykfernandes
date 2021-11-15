02edf656cd2c26d5651a0c77eba8588899bea4f0
Anny K Fernandes <78033190+annykfernandes@users.noreply.github.com>
2021-11-15 03:39:27 -0300

    Update Player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,9 +1,10 @@
 #include "../include/Player.hpp"
 
 std::string level;
+int lv;
 std::string name;
 std::vector<std::string> words;
-int score;
+int score = 1;
 
 
 Player::Player() 
@@ -11,23 +12,54 @@ Player::Player()
     level = "Fácil";
     name = "fulaninho";
     words.push_back("");
-    score = 0;
 }
+
+void Player::setLevel(int l)
+{   
+    
+    if(l == 1)
+    {
+        level = "Fácil";
+         lv = 5;
+    }
+    else if(l = 2)
+    {
+        level = "Médio";
+        lv = 10;
+    }
+    else if(l = 3)
+    {
+        level = "Difícil";
+        lv = 20;
+    }
+
+    cout<<"Nível: "<< level << endl;
+
+}
+
 void Player::setName(std::string n)
 {
     name = n;
+    cout<<"Nome: "<< name << endl;
 }
+
 std::string Player::getName()
 {   
-    std::cout<<name<<std::endl;
     return name;
 } 
 
-void Player::setScore(int s)
-{
-    score = s;
+void Player::setScore(int scr)
+{   
+    score = scr * lv;
+    cout<<"Score final: "<< score << endl;
 }
-int Player::getScore()
+
+void Player::setWords(std::vector<std::string> w)
 {
-    return score;
+    words = w;
+    cout<<"Palavras que conseguiu: "<<endl;
+    for(int i = 0; i < words.size(); i++)
+    {
+        cout<<words[i]<<endl;
+    }
 }
