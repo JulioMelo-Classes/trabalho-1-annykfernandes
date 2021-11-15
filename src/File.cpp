19131317e649e0dd9f9ab92f62f5e673e9e466d3
Anny K Fernandes <78033190+annykfernandes@users.noreply.github.com>
2021-11-15 03:40:01 -0300

    Update File.cpp

--- src/File.cpp
+++ src/File.cpp
@@ -1,6 +1,7 @@
 #include "../include/File.hpp"
 
 std::vector<std::string> file_lines;
+std::vector< std::pair<std::string, int> > pair_word_number;
 
 File::File()
 {   
@@ -24,12 +25,41 @@ std::vector<std::string> File::loadFile(std::string path)
 
 
 bool File::validateScoresFile(std::vector<std::string> scores_file)
-{     
-    return true;     
+{    
+
+    bool is_valid; 
+    for(int i = 0; i<scores_file.size(); i++)
+    {
+        std::string a = scores_file[i];
+        regex b("([a-zA-Zá-ú])+;([a-zA-Zá-ú]| |,)+;([a-zA-Zá-ú-]| |,|)+; [0-9]+$");
+        if(regex_match(a, b))
+        {
+            is_valid = true;  
+        }
+        else
+        {
+            is_valid = false;
+        }
+    }
+    return is_valid;
 }
 bool File::validateWordsFile(std::vector<std::string> words_file)
 {
-    return true;     
+    bool is_valid; 
+    for(int i = 0; i<words_file.size(); i++)
+    {
+        std::string a = words_file[i];
+        regex b("([([a-zA-Zá-ú-]{4,}+) +[0-9]+");
+        if(regex_match(a, b))
+        {
+            is_valid = true;  
+        }
+        else
+        {
+            is_valid = false;
+        }
+    }
+    return is_valid;
 }
 
 void File::getScores()
@@ -41,4 +71,60 @@ void File::getScores()
         std::cout<<file_lines[i]<<std::endl;
     }
     std::cout<<"--------------------------------------------------------------------"<<std::endl;
+}
+
+std::vector< std::pair<std::string, int> > File::generate_pairs(std::vector<std::string> w_lines)
+{   
+    for(int i = 0; i<w_lines.size(); i++)
+    {
+
+        std::string str = w_lines[i];
+
+        std::regex wrd("[^0-9 ]");
+        std::sregex_iterator m_wrd(str.begin(), str.end(), wrd);
+        std::sregex_iterator lastMatchWrd;
+
+        std::vector<std::string> v_word;
+        while(m_wrd != lastMatchWrd)
+        {
+            std::smatch sm_word = *m_wrd;
+            v_word.push_back(sm_word.str());
+            m_wrd++;
+        }
+
+        std::regex nbr("[0-9]");
+        std::sregex_iterator m_nbr(str.begin(), str.end(), nbr);
+        std::sregex_iterator lastMatchNbr;
+
+        std::vector<std::string> v_number;
+        while(m_nbr != lastMatchNbr)
+        {
+            std::smatch sm_number = *m_nbr;
+            v_number.push_back(sm_number.str());
+            m_nbr++;
+        }
+
+        std::ostringstream out_word;
+        for(std::string s: v_word)
+        {
+            out_word << s;
+        }
+        std::string word(out_word.str());
+
+        std::ostringstream out_number;
+        for(std::string n: v_number)
+        {
+            out_number << n;
+        }
+        std::string s_number(out_number.str());
+        std::stringstream ss;
+        ss<<s_number;
+        int number;
+        ss>>number;
+
+        pair_word_number.push_back(std::make_pair(word,number));
+
+    }
+    
+    return pair_word_number;
 }
