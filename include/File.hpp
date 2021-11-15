45ec6ca8cda3a03a0dada8699df767985e92db54
Anny K Fernandes <78033190+annykfernandes@users.noreply.github.com>
2021-11-15 03:42:59 -0300

    Update File.hpp

--- include/File.hpp
+++ include/File.hpp
@@ -5,18 +5,24 @@
 #include <vector>
 #include<string>
 #include <fstream>
+#include <regex>
+#include <sstream>
+#include <utility>
+
 using namespace std;
 
 class File
 {   
     public:
         std::vector<std::string> file_lines;
+        std::vector< std::pair<std::string, int> > pair_word_number;
         //Member functions
-        File();//constructor
+        File();
         std::vector<std::string> loadFile(std::string path);
         bool validateScoresFile(std::vector<std::string> scores_file);
         bool validateWordsFile(std::vector<std::string> words_file);
         void getScores();
+        std::vector< std::pair<std::string, int> > generate_pairs(std::vector<std::string> w_lines);
 };
 
 #endif
