8905cf9571bc25771af7f2d7207c2620126cb408
nnkfernandes <annykdev@gmail.com>
2021-11-17 00:54:00 -0300

    update code

---
+++ build/CMakeFiles/forca.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nico/Downloads/trabalho-1-annykfernandes-master/src/File.cpp" "CMakeFiles/forca.dir/src/File.cpp.o" "gcc" "CMakeFiles/forca.dir/src/File.cpp.o.d"
+  "/home/nico/Downloads/trabalho-1-annykfernandes-master/src/Forca.cpp" "CMakeFiles/forca.dir/src/Forca.cpp.o" "gcc" "CMakeFiles/forca.dir/src/Forca.cpp.o.d"
+  "/home/nico/Downloads/trabalho-1-annykfernandes-master/src/Player.cpp" "CMakeFiles/forca.dir/src/Player.cpp.o" "gcc" "CMakeFiles/forca.dir/src/Player.cpp.o.d"
+  "/home/nico/Downloads/trabalho-1-annykfernandes-master/src/main.cpp" "CMakeFiles/forca.dir/src/main.cpp.o" "gcc" "CMakeFiles/forca.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/forca.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/forca.dir/src/File.cpp.o"
+  "CMakeFiles/forca.dir/src/File.cpp.o.d"
+  "CMakeFiles/forca.dir/src/Forca.cpp.o"
+  "CMakeFiles/forca.dir/src/Forca.cpp.o.d"
+  "CMakeFiles/forca.dir/src/Player.cpp.o"
+  "CMakeFiles/forca.dir/src/Player.cpp.o.d"
+  "CMakeFiles/forca.dir/src/main.cpp.o"
+  "CMakeFiles/forca.dir/src/main.cpp.o.d"
+  "forca"
+  "forca.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/forca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
